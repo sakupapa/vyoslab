d0528cdc77c6b68c5bfdb554010cc9f2a81e7215
sakupapa <otakama@gmail.com>
2026-02-26 13:48:14 +0900

    deleted:    dns.sh 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 sudo brctl addbr br-dc
-sudo ip link dev br-dc up
+sudo ip link set dev br-dc up
 sudo ip addr add 100.0.0.1/24 dev br-dc
 sudo ip route add 100.1.0.0/24 via 100.0.0.254
 docker build -t br:1.0 ~/vyoslab/br
