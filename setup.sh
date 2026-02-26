bbe5199d8ea92badf73ef82349a178d9d036fd3c
sakupapa <otakama@gmail.com>
2026-02-26 13:35:15 +0900

    mod

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ sudo brctl addbr br-dc
 sudo ip link dev br-dc up
 sudo ip addr add 100.0.0.1/24 dev br-dc
 sudo ip route add 100.1.0.0/24 via 100.0.0.254
-docker build -t br:1.0 ~/br
-docker build -t nginx:1.0 ~/nginx
-docker build -t tacacs:1.0 ~/nginx
-docker build -t chrome:1.0 ~/nginx
+docker build -t br:1.0 ~/vyoslab/br
+docker build -t nginx:1.0 ~/vyoslab/nginx
+docker build -t tacacs:1.0 ~/vyoslab/tacacs
+docker build -t chrome:1.0 ~/vyoslab/chrome1
 docker pull coredns/coredns
  
