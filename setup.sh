8364445f1e4f780ade6b9793f1989c02b25ef6e0
sakupapa <otakama@gmail.com>
2026-02-26 13:24:03 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+sudo brctl addbr br-dc
+sudo ip link dev br-dc up
+sudo ip addr add 100.0.0.1/24 dev br-dc
+sudo ip route add 100.1.0.0/24 via 100.0.0.254
+docker build -t br:1.0 ~/br
+docker build -t nginx:1.0 ~/nginx
+docker build -t tacacs:1.0 ~/nginx
+docker build -t chrome:1.0 ~/nginx
+docker pull coredns/coredns
+ 
